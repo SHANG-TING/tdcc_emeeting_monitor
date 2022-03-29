62eadac3748fc0a7eaf64bc7a6f91c737800bae0
Neil Xie <neil@miniasp.com>
2022-03-29 14:51:35 +0800

    initial commit

---
+++ apps/tdcc-emeeting-monitor/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tdcc-emeeting-monitor-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'tdcc-emeeting-monitor';
+}
